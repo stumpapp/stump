b90f4b5a298ec987f768272c0d8c82ad5c5abe26
Aaron Leopold <aaronleopold1221@gmail.com>
2022-07-30 14:43:51 -0700

    bah humbug

--- core/src/config/context.rs
+++ core/src/config/context.rs
@@ -36,7 +36,6 @@ impl Ctx {
 		Ctx {
 			db: Arc::new(db::create_client().await),
 			internal_sender: Arc::new(unbounded_channel::<ClientRequest>().0),
-			// task_sender: Arc::new(unbounded_channel::<TaskResponder<InternalTask>>().0),
 			response_channel: Arc::new(channel::<ClientEvent>(1024)),
 		}
 	}

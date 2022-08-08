9adf7c7e2b63ff730bf06a7c896bde1feeb152cb
Aaron Leopold <aaronleopold1221@gmail.com>
2022-08-08 15:43:45 -0700

    error fallback design

--- apps/client/src/components/ErrorBoundary.tsx
+++ apps/client/src/components/ErrorBoundary.tsx
@@ -1,41 +1,54 @@
-import { Code, Heading, Link, Stack, Text, useBoolean } from '@chakra-ui/react';
+import { Code, Heading, HStack, Link, Spacer, Stack, Text, useBoolean } from '@chakra-ui/react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import Button from '~ui/Button';
 import BaseLayout from './Layouts/BaseLayout';
 
 function ErrorFallback({ error }: FallbackProps) {
-	const [showMore, { toggle }] = useBoolean(false);
+	// const [showMore, { toggle }] = useBoolean(true);
 
 	return (
 		<BaseLayout>
-			<Stack mt={{ base: 12, md: 16 }} w="full" h="full" align="center">
-				<Heading as="h1" size="lg">
+			<Stack mt={{ base: 12, md: 16 }} w="full" h="full" align="center" spacing={3}>
+				<Heading as="h4" size="sm">
 					Well, this is embarrassing...
 				</Heading>
 
-				<Text fontSize="lg" maxW="xl" textAlign="center">
-					Something went wrong! Error: {error.message}.{' '}
-					{error.stack && (
+				<Heading as="h2" size="lg">
+					Something went wrong!
+				</Heading>
+
+				<Text as="pre" fontSize="lg" maxW="xl" textAlign="center" pt={4}>
+					Error: {error.message}.{' '}
+					{/* {error.stack && (
 						<>
 							<span onClick={toggle} className="cursor-pointer text-brand-400">
 								Click me
 							</span>{' '}
 							to see {showMore ? 'less' : 'more'}.
 						</>
-					)}
+					)} */}
 				</Text>
 
-				{showMore && (
-					<Code rounded="md" maxW="4xl" p={4}>
+				{error.stack && (
+					<Code rounded="md" maxW="4xl" maxH={72} overflowY="scroll" p={4}>
 						{error.stack}
 					</Code>
 				)}
 
-				<Text fontSize="lg" maxW="xl" textAlign="center">
-					<Link href="/" color="brand.400">
-						Click here
-					</Link>{' '}
-					to go home
-				</Text>
+				<HStack pt={3}>
+					<Button
+						href="https://github.com/aaronleopold/stump/issues/new/choose"
+						target="_blank"
+						className="!no-underline"
+						as={Link}
+					>
+						Report Bug
+					</Button>
+
+					<Button href="/" variant="solid" colorScheme="brand" className="!no-underline" as={Link}>
+						Go Home
+					</Button>
+				</HStack>
 			</Stack>
 		</BaseLayout>
 	);

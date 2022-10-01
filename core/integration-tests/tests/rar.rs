c689ed28f7691165e6c3b99c8a4d0ebdf904233a
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2022-10-01 10:48:04 -0700

    WIP: tests

--- core/integration-tests/tests/rar.rs
+++ core/integration-tests/tests/rar.rs
@@ -1,7 +1,6 @@
 use rocket::tokio;
-use std::path::Path;
 
-use crate::utils::init_test;
+use crate::utils::{init_test, make_tmp_file, TempLibrary};
 
 use stump_core::{
 	config::Ctx,
@@ -10,7 +9,7 @@ use stump_core::{
 		rar::{convert_rar_to_zip, rar_sample},
 	},
 	prisma::media,
-	types::CoreResult,
+	types::{CoreResult, LibraryPattern, LibraryScanMode},
 };
 
 // TODO: fix these tests...
@@ -19,69 +18,35 @@ use stump_core::{
 // TODO: don't ignore, need to figure out best way to do this... something like the
 // tempfile crate maybe?
 #[ignore]
-fn test_rar_to_zip() {
-	let test_file = "/Users/aaronleopold/Documents/Stump/Demo/Venom/Venom 001 (2022).cbr";
+fn test_rar_to_zip() -> CoreResult<()> {
+	let tmp_file = make_tmp_file("book.rar")?;
 
-	let path = Path::new(test_file);
+	let path = tmp_file.path();
 
 	let result = convert_rar_to_zip(path);
-
-	// assert!(result.is_ok());
+	assert!(result.is_ok());
 
 	let zip_path = result.unwrap();
+	assert!(zip_path.exists());
 
-	// assert!(zip_path.exists());
-
-	println!("{:?}", zip_path);
-}
-
-#[tokio::test]
-async fn digest_rars_asynchronous() -> CoreResult<()> {
-	init_test().await;
-
-	let ctx = Ctx::mock().await;
-
-	let rars = ctx
-		.db
-		.media()
-		.find_many(vec![media::extension::in_vec(vec![
-			"rar".to_string(),
-			"cbr".to_string(),
-		])])
-		.exec()
-		.await?;
-
-	// TODO: uncomment once I create rar test data
-	// assert_ne!(rars.len(), 0);
-
-	// TODO: remove this check once I create rar test data
-	if rars.len() == 0 {
-		println!("STINKY: could not run digest_rars_asynchronous test until aaron fixes his stuff");
-		return Ok(());
-	}
-
-	for rar in rars {
-		let rar_sample_result = rar_sample(&rar.path);
-
-		assert!(rar_sample_result.is_ok());
-
-		let rar_sample = rar_sample_result.unwrap();
-
-		let digest_result = checksum::digest_async(&rar.path, rar_sample).await;
-		assert!(digest_result.is_ok());
-
-		let checksum = digest_result.unwrap();
-		assert_ne!(checksum.len(), 0);
-	}
+	// TODO: more?
 
 	Ok(())
 }
 
 #[tokio::test]
+#[ignore]
 async fn digest_rars_synchronous() -> CoreResult<()> {
 	init_test().await;
 
 	let ctx = Ctx::mock().await;
+
+	let _ret = TempLibrary::create(
+		ctx.get_db(),
+		LibraryPattern::SeriesBased,
+		LibraryScanMode::Batched,
+	)
+	.await?;
 
 	let rars = ctx
 		.db
--- core/src/fs/checksum.rs
+++ core/src/fs/checksum.rs
@@ -1,10 +1,6 @@
 use data_encoding::HEXLOWER;
 use ring::digest::{Context, SHA256};
-use rocket::tokio::{
-	fs::File,
-	io::{self, AsyncReadExt, AsyncSeekExt, SeekFrom},
-};
-use std::io::Read;
+use std::io::{self, Read};
 
 // use std::fs::File;
 #[cfg(target_family = "unix")]
@@ -17,20 +13,6 @@ use crate::CoreError;
 
 pub const DIGEST_SAMPLE_SIZE: u64 = 10000;
 pub const DIGEST_SAMPLE_COUNT: u64 = 4;
-
-async fn read_async(
-	file: &mut File,
-	offset: u64,
-	size: u64,
-) -> Result<Vec<u8>, io::Error> {
-	let mut buffer = vec![0u8; size as usize];
-
-	// seek to the appropriate offset before reading to buffer
-	file.seek(SeekFrom::Start(offset)).await?;
-	file.read_exact(&mut buffer).await?;
-
-	Ok(buffer)
-}
 
 fn read(file: &std::fs::File, offset: u64, size: u64) -> Result<Vec<u8>, io::Error> {
 	let mut buffer = vec![0u8; size as usize];
@@ -46,37 +28,6 @@ fn read(file: &std::fs::File, offset: u64, size: u64) -> Result<Vec<u8>, io::Err
 	}
 
 	Ok(buffer)
-}
-
-pub async fn digest_async(path: &str, bytes: u64) -> Result<String, io::Error> {
-	let mut file = File::open(path).await?;
-
-	let mut ring_context = Context::new(&SHA256);
-
-	if bytes <= DIGEST_SAMPLE_SIZE * DIGEST_SAMPLE_COUNT {
-		let buffer = read_async(&mut file, 0, bytes).await?;
-		ring_context.update(&buffer);
-	} else {
-		for i in 0..DIGEST_SAMPLE_COUNT {
-			let offset = (bytes / DIGEST_SAMPLE_COUNT) * i;
-
-			let buffer = read_async(&mut file, offset, DIGEST_SAMPLE_SIZE).await?;
-			ring_context.update(&buffer);
-		}
-		let offset = bytes - DIGEST_SAMPLE_SIZE;
-
-		let buffer = read_async(&mut file, offset, DIGEST_SAMPLE_SIZE).await?;
-
-		ring_context.update(&buffer);
-	}
-
-	let digest = ring_context.finish();
-
-	let encoded_digest = HEXLOWER.encode(digest.as_ref());
-
-	log::debug!("Generated checksum: {:?}", encoded_digest);
-
-	Ok(encoded_digest)
 }
 
 pub fn digest(path: &str, bytes: u64) -> Result<String, io::Error> {

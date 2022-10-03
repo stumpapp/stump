54f0bba1fef7f1c9f086d4b2f7277df88402aaf7
Aaron Leopold <aaronleopold1221@gmail.com>
2022-10-03 15:28:45 -0700

    fix baseUrl in release profile

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,9 +3,11 @@ import { StumpQueryProvider } from '@stump/client';
 
 import '@stump/interface/styles';
 
-export const baseUrl = import.meta.env.PROD
-	? `${import.meta.env.BASE_URL}`
-	: 'http://localhost:10801';
+// export const baseUrl = import.meta.env.PROD
+// 	? `${import.meta.env.BASE_URL}`
+// 	: 'http://localhost:10801';
+
+export const baseUrl = import.meta.env.PROD ? window.location.href : 'http://localhost:10801';
 
 export default function App() {
 	return (

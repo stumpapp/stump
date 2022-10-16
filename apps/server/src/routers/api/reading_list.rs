d5b5ac15026754fb8cd3d3351c94c89a4a33a478
berkingurcan <berkingurcan@yandex.com>
2022-10-16 16:51:01 +0300

    feat: add get_reading_list

--- apps/server/src/routers/api/reading_list.rs
+++ apps/server/src/routers/api/reading_list.rs
@@ -4,8 +4,15 @@ use axum::{
 };
 use axum_sessions::extractors::{ReadableSession, WritableSession};
 use stump_core::{
-    prisma::{reading_list, media}
+    prisma::{reading_list, media, user},
+	types::{User, ReadingList},
 };
+use crate::{
+	config::state::State,
+	errors::{ApiError, ApiResult},
+	utils::{get_session_user},
+};
+
 
 pub(crate) fn mount() -> Router {
     Router::new()
@@ -17,8 +24,28 @@ pub(crate) fn mount() -> Router {
         )
 }
 
-async fn get_reading_list() {
-    todo!()
+async fn get_reading_list(
+    Extension(ctx): State,
+	session: ReadableSession,
+) -> ApiResult<Json<Vec<ReadingList>>> {
+	let user = get_session_user(&session)?;
+
+    if user != user.reading_lists.creating_user {
+        return Err(ApiError::Forbidden(
+			"You do not have permission to access this resource.".into(),
+		));
+    }
+
+    Ok(Json(
+        ctx.db
+            .reading_list()
+            .find_many(vec![])
+			.exec()
+			.await?
+			.into_iter()
+			.map(|u| u.into())
+			.collect::<Vec<ReadingList>>(),
+    ))
 }
 
 async fn create_reading_list() {

ad28224020a2aa5797b85b5f3b0a3e90bfa31a3b
berkingurcan <berkingurcan@yandex.com>
2022-10-19 08:02:37 +0300

    feat: add get reading list by id

--- apps/server/src/routers/api/reading_list.rs
+++ apps/server/src/routers/api/reading_list.rs
@@ -1,5 +1,6 @@
 use axum::{
 	routing::{get, post, put, delete},
+    extract::Path,
 	Extension, Json, Router,
 };
 use axum_sessions::extractors::{ReadableSession, WritableSession};
@@ -64,8 +65,28 @@ async fn create_reading_list(
     Ok(Json(created_reading_list.into()))
 }
 
-async fn get_reading_list_by_id() {
-    todo!()
+async fn get_reading_list_by_id(
+	Path(id): Path<String>,
+    Extension(ctx): State,
+	session: ReadableSession,
+) -> ApiResult<Json<ReadingList>> {
+	let user_id = get_session_user(&session)?.id;
+	let db = ctx.get_db();
+
+    let reading_list_id = db
+        .reading_list()
+        .find_unique(reading_list::id::equals(id.clone()))
+        .exec()
+        .await?;
+
+    if reading_list_id.is_none() {
+        return Err(ApiError::NotFound(format!(
+			"Reading List with id {} not found",
+			id
+		)));
+    }
+    
+    Ok(Json(reading_list_id.unwrap().into()))
 }
 
 async fn update_reading_list() {

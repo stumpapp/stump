449e28749cafb6673d2a9146214b8cd4cf45cce8
Berkin Gürcan <berkingurcan@yandex.com>
2022-10-20 17:13:48 +0300

    Develop (#76)
    * feat: create reading_list route file
    
    * feat: add prisma model and core/types/model
    
    types model will be completed
    
    * feat: add get_reading_list
    
    * fix: handle compile errors
    
    * Update reading_list.rs
    
    * feat: add create reading list post method
    
    * feat: add createreadinglist struct
    
    * fix: parameters of create in create_reading_list
    
    But, it has error[E0603]: module `user` is private:
    reading_list::creating_user::user::id::equals(user_id),
         |                                          ^^^^ private module
    
    * Update reading_list.rs
    
    * fix: handled creating_user parameter
    
    * fix: unused line
    
    * feat: add get reading list by id
    
    * feat: add update_reading_list by id
    
    * feat: add delete reading list
    
    * fix: unused imports and paranthesis
    
    * fix: some typos

---
+++ core/src/types/models/readinglist.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+use specta::Type;
+
+use crate::prisma::{self};
+
+#[derive(Debug, Clone, Serialize, Deserialize, Type)]
+pub struct ReadingList {
+	pub id: String,
+	pub name: String,
+	pub creating_user_id: String,
+	pub description: Option<String>,
+}
+
+impl From<prisma::reading_list::Data> for ReadingList {
+	fn from(data: prisma::reading_list::Data) -> ReadingList {
+		ReadingList {
+			id: data.id,
+			name: data.name,
+			creating_user_id: data.creating_user_id,
+			description: data.description,
+		}
+	}
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Type)]
+pub struct CreateReadingList {
+	pub id: String,
+	pub media_ids: Vec<String>
+}

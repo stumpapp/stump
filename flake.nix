1e31d2c42356e3595074d2e927a256130803ccc3
chris <christo1771@gmail.com>
2022-12-31 16:45:59 -0500

    Nix flakes for stump

--- flake.nix
+++ flake.nix
@@ -2,12 +2,16 @@
   inputs = {
     nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
 
         libraries = with pkgs;[
           webkitgtk
@@ -25,6 +29,10 @@
           nodejs
 
           # rust
+          rustfmt
+          clippy
+          rustc
+          cargo
           cargo-deny
           cargo-edit
           cargo-watch
@@ -43,7 +51,15 @@
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = packages;
+          buildInputs = packages ++ [(
+            pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+            }
+          )];
+
+          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+          }}/lib/rustlib/src/rust/library";
 
           shellHook =
             ''

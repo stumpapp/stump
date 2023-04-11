7d65f5b87eb74dd0f483d5f34043f435dffa225e
Daniele Cambi <dancam.dev@gmail.com>
2023-04-11 19:28:32 +0200

    rebase

---
+++ apps/mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function App() {
+	return (
+		<View className="flex flex-1 justify-center items-center">
+			<Text className="text-gray-950">NativeWind and TS Working</Text>
+		</View>
+	)
+}

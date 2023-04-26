bda72120b7bd0d48fe404d4fa08df37a73a9f068
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2023-04-25 17:52:08 -0700

    fun splash screen

--- apps/desktop/src-tauri/src/commands.rs
+++ apps/desktop/src-tauri/src/commands.rs
@@ -1,5 +1,7 @@
 use std::sync::Mutex;
 
+use tauri::Manager;
+
 use crate::utils::discord::StumpDiscordPresence;
 
 // TODO: error handling :D
@@ -30,4 +32,12 @@ pub fn set_discord_presence(
 	if client.is_connected() {
 		client.set_presence(status.as_deref(), details.as_deref());
 	}
+}
+
+#[tauri::command]
+pub async fn close_splashscreen(window: tauri::Window) {
+	if let Some(splashscreen) = window.get_window("splashscreen") {
+		splashscreen.close().unwrap();
+	}
+	window.get_window("main").unwrap().show().unwrap();
 }

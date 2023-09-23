9b455e64025a3dcd474ffec1872590ed409e907f
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2023-09-23 09:36:46 -0700

    make all available languages selectable

--- packages/interface/src/i18n/index.ts
+++ packages/interface/src/i18n/index.ts
@@ -4,10 +4,34 @@ export { LocaleContext, type LocaleContextProps, useLocaleContext } from './cont
 export { default as LocaleProvider } from './LocaleProvider'
 
 export const localeNames: Record<AllowedLocale, string> = {
+	af: 'Afrikaans',
+	ar: 'العربية',
+	ca: 'Català',
+	cs: 'Čeština',
+	da: 'Dansk',
+	de: 'Deutsch',
+	el: 'Ελληνικά',
 	en: 'English',
 	es: 'Español',
+	fi: 'Suomi',
 	fr: 'Français',
+	he: 'עברית',
+	hu: 'Hungarian',
 	it: 'Italiano',
+	ja: '日本語',
+	ko: '한국어',
+	nl: 'Nederlands',
+	no: 'Norsk',
+	pl: 'Polski',
+	pt: 'Português',
+	ro: 'Română',
+	ru: 'Русский',
+	sr: 'Srpski',
+	sv: 'Svenska',
+	tr: 'Türkçe',
+	uk: 'Українська',
+	vi: 'Tiếng Việt',
+	zh: '中文',
 }
 
 export function isLocale(value: string): value is AllowedLocale {

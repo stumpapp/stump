3dcddd010850ed425f14dcdd2c074592a77a68ea
LRotenberger <62265343+LRotenberger@users.noreply.github.com>
2023-10-11 19:19:31 -0400

    🚧 Add integrations crate (#175)
    * Started work on discord integration
    
    * Finished discord client notifier, added scan complete event
    
    * Added telegram client framework
    
    * changed readme
    
    * standardized test variables
    
    * adjustment for rust function pnpmnaming rules

---
+++ crates/integrations/src/notifier/error.rs
@@ -0,0 +1,11 @@
+pub type NotifierResult<T> = Result<T, NotifierError>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum NotifierError {
+	#[error("Request failed with error: {0}")]
+	ReqwestError(#[from] reqwest::Error),
+	#[error("{0}")]
+	Unimplemented(String),
+	#[error("Request was unsucessful")]
+	RequestFailed(String),
+}
---
+++ crates/integrations/src/notifier/mod.rs
@@ -0,0 +1,20 @@
+mod discord_client;
+mod error;
+mod event;
+mod telegram_client;
+
+pub use discord_client::DiscordClient;
+pub use event::NotifierEvent;
+pub use telegram_client::TelegramClient;
+
+use self::error::NotifierResult;
+
+pub const NOTIFIER_ID: &str = "Stump Notifier";
+pub const FAVICON_URL: &str = "https://stumpapp.dev/favicon.png";
+
+#[async_trait::async_trait]
+pub trait Notifier {
+	// TODO: MessageConfig struct? So we can style according to NotifierEvent?
+	fn payload_from_event(event: NotifierEvent) -> NotifierResult<serde_json::Value>;
+	async fn send_message(&self, event: NotifierEvent) -> NotifierResult<()>;
+}

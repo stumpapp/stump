1048b48939cadba57b4919bf861500529b71cd44
LRotenberger <logarotenberger@gmail.com>
2024-02-09 16:31:52 -0500

    WIP: get mobile app working

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,12 +1,11 @@
 module.exports = function (api) {
 	api.cache(true)
 	return {
-		presets: ['babel-preset-expo'],
 		plugins: [
 			'@babel/plugin-proposal-export-namespace-from',
 			'react-native-reanimated/plugin',
-			require.resolve('expo-router/babel'),
 			'nativewind/babel',
 		],
+		presets: ['babel-preset-expo'],
 	}
 }

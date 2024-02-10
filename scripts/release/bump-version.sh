96b29fe0e3178493bd0ca616ca28da3adb36f5c8
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2024-02-10 11:48:10 -0700

    👷 Define release workflow (#263)
    * Add logic to scratch version script
    
    * WIP: start moving unused nightly steps to release ci
    
    * Does this work?
    
    * add nightly
    
    * ugh
    
    * Try sed command
    
    * Name
    
    kms
    
    * remove if for nightly

--- scripts/release/bump-version.sh
+++ scripts/release/bump-version.sh
@@ -2,26 +2,37 @@
 
 # https://github.com/pksunkara/cargo-workspaces#version
 _BUMP=${BUMP:?bump is required}
-_GENERATE_CHANGELOG=${GENERATE_CHANGELOG:=0}
+_GENERATE_CHANGELOG=${GENERATE_CHANGELOG:=1}
 
-# desktop,mobile,core
-_TARGETS=${TARGETS:=$@}
+# desktop, mobile, core -> default is core
+_TARGETS=${TARGETS:=core}
 
-# SCRIPTS_DIR="${BASH_SOURCE%/*}/.."
-# source "${SCRIPTS_DIR}/lib"
-
-# TODO: implement some sort of release system! This is all scratch work
-
-
-# if no targets, exit
+# If no targets are specified, exit. This _shouldn't_ happen since the default is core
 if [ -z "$_TARGETS" ]; then
   echo "No targets specified. Exiting..."
   exit 1
 fi
 
-# TODO: bump based on targets!!
 
-# cargo workspaces version $BUMP --no-git-commit
+if [[ $_TARGETS == *"core"* ]]; then
+ # Bump the Cargo workspaces version, this is interactive
+  cargo workspaces version $BUMP --no-git-commit
+
+  # Get the current version in the root package.json
+  _VERSION=$(jq -r '.version' package.json)
+
+  # Update the version according to the bump (major, minor, patch)
+  _NEW_VERSION=$(pnpx semver $_VERSION -i $_BUMP)
+
+  # Update the version in the root package.json and in: (apps/*/package.json, packages/*/package.json, interface/package.json)
+  PATHS=("package.json" "apps/*/package.json" "packages/*/package.json" "interface/package.json")
+  for path in ${PATHS[@]}; do
+    jq ".version = \"$_NEW_VERSION\"" $path > tmp.$$.json && mv tmp.$$.json $path
+  done
+
+  # Fix the formatting of the JSON files from the previous step 
+  pnpm prettify
+fi
 
 # https://docs.gitmoji-changelog.dev/#/?id=%f0%9f%93%9a-how-it-works
 if [ $_GENERATE_CHANGELOG == 1 ]; then

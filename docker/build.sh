6ea21d026dbaf7eaeb67a3bb1654c4da6567a32f
Aaron Leopold <aaronleopold1221@gmail.com>
2024-08-24 10:42:10 -0700

    HAHA

--- docker/build.sh
+++ docker/build.sh
@@ -9,12 +9,11 @@ _RUN_PRISMA_GENERATE=${RUN_PRISMA_GENERATE:=false}
 _GIT_REV=${GIT_REV:-$(git rev-parse --short HEAD)}
 
 FORMATTED_TAGS=""
-for tag in ${_TAGS//,/ }; do
-  FORMATTED_TAGS="$FORMATTED_TAGS -t $tag"
+for tag in ${_TAGS//,/}; do
+  FORMATTED_TAGS="$FORMATTED_TAGS --tag $tag"
 done
 
 echo "Building with tag arguments: $FORMATTED_TAGS"
-echo "Current working directory: $(pwd)"
 
 set -ex; \
 ${_RUNTIME} buildx build \
@@ -22,7 +21,6 @@ ${_RUNTIME} buildx build \
   --load \
   --progress=$_FORMAT \
   --platform=$_PLATFORMS \
-  # -t aaronleopold/stump:$_TAG \
-  $FORMATTED_TAGS \
+  ${FORMATTED_TAGS} \
   --build-arg GIT_REV=$_GIT_REV \
   --build-arg RUN_PRISMA_GENERATE=$_RUN_PRISMA_GENERATE .

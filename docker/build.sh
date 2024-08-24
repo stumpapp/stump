1f962f80e8fbcce3f36a88ee72368286bd43167e
Aaron Leopold <aaronleopold1221@gmail.com>
2024-08-24 10:23:11 -0700

    try use podman?

--- docker/build.sh
+++ docker/build.sh
@@ -3,8 +3,16 @@
 _RUNTIME=${RUNTIME:-docker}
 _FORMAT=${FORMAT:-auto}
 _PLATFORMS=${PLATFORMS:-linux/amd64}
-_TAG=${TAG:-nightly}
+# _TAG=${TAG:-nightly}
+_TAGS=${TAGS:-"aaronleopold/stump:nightly"}
 _RUN_PRISMA_GENERATE=${RUN_PRISMA_GENERATE:=false}
+
+FORMATTED_TAGS=""
+for tag in ${_TAGS//,/ }; do
+  FORMATTED_TAGS="$FORMATTED_TAGS -t $tag"
+done
+
+echo "Building with tag arguments: $FORMATTED_TAGS"
 
 set -ex; \
 ${_RUNTIME} buildx build \
@@ -12,6 +20,7 @@ ${_RUNTIME} buildx build \
   --load \
   --progress=$_FORMAT \
   --platform=$_PLATFORMS \
-  -t aaronleopold/stump:$_TAG \
+  # -t aaronleopold/stump:$_TAG \
+  $FORMATTED_TAGS \
   --build-arg GIT_REV=$(git rev-parse --short HEAD) \
   --build-arg RUN_PRISMA_GENERATE=$_RUN_PRISMA_GENERATE .

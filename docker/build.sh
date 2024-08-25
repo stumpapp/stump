6a16235bd4f1552d285c39e4b150c7b5a189ad32
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2024-08-25 11:00:54 -0700

    :construction_worker: Switch to `podman` and introduce better caching (#416)
    * here we go
    
    * try removing --mount=type=cache
    
    * try (and fail) to get podman working
    
    it keeps saying cargo not found?????
    
    * oh
    
    * try use podman?
    
    * debug
    
    * debug
    
    * kicking screaming crying
    
    * HAHA
    
    * changes after messing with runners
    
    * test code change
    
    * clean up
    
    * remove debug comments
    
    * try on small runner
    
    * restore workflow args and broader label for runs-on
    
    * remove duplicate

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+_ENGINE=${ENGINE:-docker}
 _FORMAT=${FORMAT:-auto}
 _PLATFORMS=${PLATFORMS:-linux/amd64}
-_TAG=${TAG:-nightly}
+_TAGS=${TAGS:-"aaronleopold/stump:nightly"}
 _RUN_PRISMA_GENERATE=${RUN_PRISMA_GENERATE:=false}
+_GIT_REV=${GIT_REV:-$(git rev-parse --short HEAD)}
 
-docker buildx build -f ./docker/Dockerfile --load --progress=$_FORMAT --platform=$_PLATFORMS -t aaronleopold/stump:$_TAG --build-arg GIT_REV=$(git rev-parse --short HEAD) --build-arg RUN_PRISMA_GENERATE=$_RUN_PRISMA_GENERATE .
+FORMATTED_TAGS=""
+for tag in ${_TAGS//,/}; do
+  FORMATTED_TAGS="$FORMATTED_TAGS --tag $tag"
+done
+
+echo "Building with tag arguments: $FORMATTED_TAGS"
+
+set -ex; \
+${_ENGINE} buildx build \
+  -f ./docker/Dockerfile \
+  --load \
+  --progress=$_FORMAT \
+  --platform=$_PLATFORMS \
+  ${FORMATTED_TAGS} \
+  --build-arg GIT_REV=$_GIT_REV \
+  --build-arg RUN_PRISMA_GENERATE=$_RUN_PRISMA_GENERATE .

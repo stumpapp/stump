07cbe75d354b6150595b7eae5353142acf675c2e
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2024-08-29 17:11:51 -0700

    :green_heart: Fix docker build push

--- docker/build.sh
+++ docker/build.sh
@@ -6,18 +6,24 @@ _PLATFORMS=${PLATFORMS:-linux/amd64}
 _TAGS=${TAGS:-"aaronleopold/stump:nightly"}
 _RUN_PRISMA_GENERATE=${RUN_PRISMA_GENERATE:=false}
 _GIT_REV=${GIT_REV:-$(git rev-parse --short HEAD)}
+_PUSH=${PUSH:-false}
 
 FORMATTED_TAGS=""
 for tag in ${_TAGS//,/}; do
   FORMATTED_TAGS="$FORMATTED_TAGS --tag $tag"
 done
 
+PUSH_OR_LOAD_ARG="--load"
+if [ "$_PUSH" = true ]; then
+  PUSH_OR_LOAD_ARG="--push"
+fi
+
 echo "Building with tag arguments: $FORMATTED_TAGS"
 
 set -ex; \
 ${_ENGINE} buildx build \
   -f ./docker/Dockerfile \
-  --load \
+  ${PUSH_OR_LOAD_ARG} \
   --progress=$_FORMAT \
   --platform=$_PLATFORMS \
   ${FORMATTED_TAGS} \

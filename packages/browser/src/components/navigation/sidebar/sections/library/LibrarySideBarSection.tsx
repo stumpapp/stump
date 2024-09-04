61b5a83e4716d38f1d1715aa4568e3899fefe46d
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2024-09-03 21:18:30 -0700

    fix ui

--- packages/browser/src/components/navigation/sidebar/sections/library/LibrarySideBarSection.tsx
+++ packages/browser/src/components/navigation/sidebar/sections/library/LibrarySideBarSection.tsx
@@ -10,8 +10,8 @@ import paths from '@/paths'
 
 import { EntityOptionProps } from '../../../types'
 import SideBarButtonLink from '../../SideBarButtonLink'
-import LibraryConfigMenu from './LibraryConfigMenu'
 import LibraryEmoji from './LibraryEmoji'
+import LibraryOptionsMenu from './LibraryOptionsMenu'
 
 type Props = {
 	isMobile?: boolean
@@ -60,7 +60,7 @@ export default function LibrarySideBarSection({
 					isActive={isCurrentLibrary(library.id)}
 					className="pl-2 pr-0"
 					leftContent={canChange ? leftContent : undefined}
-					rightContent={<LibraryConfigMenu library={library} />}
+					rightContent={<LibraryOptionsMenu library={library} />}
 					// onMouseEnter={() => prefetchLibrarySeries(library.id)}
 				>
 					{!canChange && leftContent}

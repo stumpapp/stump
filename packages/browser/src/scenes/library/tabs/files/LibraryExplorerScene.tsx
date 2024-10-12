522ba15055e9d37e92ce17c6af53ea679e78d8e7
Joseph Micheli <josephwmicheli@gmail.com>
2024-10-11 23:40:41 -0500

    Add janky test interface

--- packages/browser/src/scenes/library/tabs/files/LibraryExplorerScene.tsx
+++ packages/browser/src/scenes/library/tabs/files/LibraryExplorerScene.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@stump/components'
+import { useRef } from 'react'
+
 import { FileExplorer } from '@/components/explorer'
 
 import { useLibraryContext } from '../../context'
@@ -7,8 +10,30 @@ export default function LibraryExplorerScene() {
 		library: { path },
 	} = useLibraryContext()
 
+	const fileInputRef = useRef(null)
+
+	// TODO - Remove this test code
+	// ////////////////////////////
+	const onUploadClicked = () => {
+		// eslint-disable-next-line no-console
+		console.log(`Selected file: ${fileInputRef}`)
+	}
+
+	const onFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (!event.target.files) {
+			return
+		}
+
+		const file = event.target.files[0]
+		if (file) {
+			// Handle file here
+		}
+	}
+
 	return (
 		<div className="flex flex-1">
+			<Button onClick={onUploadClicked}>Upload</Button>
+			<input type="file" ref={fileInputRef} onChange={onFileSelected} title="Browse" />
 			<FileExplorer rootPath={path} />
 		</div>
 	)

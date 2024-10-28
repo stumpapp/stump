d4aa4c4f8258dca228c25b50908df654ba4977b8
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2024-10-28 11:47:11 -0700

    use translations for api key root

--- packages/browser/src/scenes/settings/app/apiKeys/APIKeySettingsScene.tsx
+++ packages/browser/src/scenes/settings/app/apiKeys/APIKeySettingsScene.tsx
@@ -1,4 +1,5 @@
 import { Heading, Text } from '@stump/components'
+import { useLocaleContext } from '@stump/i18n'
 import { Suspense } from 'react'
 
 import { ContentContainer, SceneContainer } from '@/components/container'
@@ -6,17 +7,18 @@ import { ContentContainer, SceneContainer } from '@/components/container'
 import APIKeyTable from './APIKeyTable'
 import CreateAPIKeyModal from './CreateAPIKeyModal'
 
-// TODO(koreader): localize
 export default function APIKeySettingsScene() {
+	const { t } = useLocaleContext()
+
 	return (
 		<SceneContainer>
 			<ContentContainer>
 				<div className="flex flex-col gap-4">
 					<div className="flex items-end justify-between">
 						<div>
-							<Heading size="sm">Your keys</Heading>
+							<Heading size="sm">{t(getKey('title'))}</Heading>
 							<Text size="sm" variant="muted" className="mt-1">
-								These are the API keys you have created
+								{t(getKey('description'))}
 							</Text>
 						</div>
 
@@ -31,3 +33,6 @@ export default function APIKeySettingsScene() {
 		</SceneContainer>
 	)
 }
+
+const LOCALE_BASE = 'settingsScene.app/apiKeys.sections.table'
+const getKey = (key: string) => `${LOCALE_BASE}.${key}`

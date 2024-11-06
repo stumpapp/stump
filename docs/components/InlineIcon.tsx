8dcf0062deaba24c8594b56bcc1e1b4904cd0fcf
Joseph Micheli <josephwmicheli@gmail.com>
2024-11-05 19:17:10 -0600

    Jazz up the upload guide

---
+++ docs/components/InlineIcon.tsx
@@ -0,0 +1,19 @@
+// InlineIcon.tsx
+import React from 'react'
+
+interface InlineIconProps {
+	children: React.ReactNode
+	size?: number
+}
+
+export default function InlineIcon(props: InlineIconProps) {
+	const size = props.size ? props.size : 16
+
+	return (
+		<span style={{ display: 'inline-flex', alignItems: 'center' }}>
+			{React.cloneElement(props.children as React.ReactElement, { size })}
+		</span>
+	)
+}
+
+//, { props.size }

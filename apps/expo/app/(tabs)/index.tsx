5c4e05557d5c4f1b126e0c1f3d276da6dcf88332
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2025-01-26 13:47:42 -0700

    scrollview for root

--- apps/expo/app/(tabs)/index.tsx
+++ apps/expo/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'expo-router'
 import partition from 'lodash/partition'
 import { View } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { icons, Text } from '~/components/ui'
 import { useSavedServers } from '~/stores'
@@ -42,70 +44,74 @@ export default function Screen() {
 	}
 
 	return (
-		<View className="flex-1 items-start justify-start gap-5 bg-background p-6">
-			<View className="flex w-full items-start gap-2">
-				<Text className="text-foreground-muted">Stump</Text>
+		<SafeAreaView className="flex-1 bg-background">
+			<ScrollView>
+				<View className="flex-1 items-start justify-start gap-5 bg-background px-6">
+					<View className="flex w-full items-start gap-2">
+						<Text className="text-foreground-muted">Stump</Text>
 
-				{!stumpServers.length && (
-					<View className="h-24 w-full items-center justify-center gap-2 rounded-lg border border-dashed border-edge p-3">
-						<View className="relative flex justify-center">
-							<View className="flex items-center justify-center rounded-lg bg-background-surface p-2">
-								<Server className="h-6 w-6 text-foreground-muted" />
-								<Slash className="absolute h-6 w-6 scale-x-[-1] transform text-foreground opacity-80" />
+						{!stumpServers.length && (
+							<View className="h-24 w-full items-center justify-center gap-2 rounded-lg border border-dashed border-edge p-3">
+								<View className="relative flex justify-center">
+									<View className="flex items-center justify-center rounded-lg bg-background-surface p-2">
+										<Server className="h-6 w-6 text-foreground-muted" />
+										<Slash className="absolute h-6 w-6 scale-x-[-1] transform text-foreground opacity-80" />
+									</View>
+								</View>
+
+								<Text>No Stump servers added</Text>
 							</View>
-						</View>
+						)}
 
-						<Text>No Stump servers added</Text>
+						{stumpServers.map((server) => (
+							<Link
+								key={server.id}
+								href={`/server/${server.id}`}
+								className="bg-background-muted w-full items-center rounded-lg border border-edge bg-background-surface p-3"
+							>
+								<View className="flex-1 items-start justify-center gap-1">
+									<Text className="text-lg">{server.name}</Text>
+									<Text className="text-foreground-muted">
+										{MASK_URLS
+											? server.url.replace(/(https?:\/\/)(.*?)(\/.*)/, '$1$2/...')
+											: server.url}
+									</Text>
+								</View>
+							</Link>
+						))}
 					</View>
-				)}
 
-				{stumpServers.map((server) => (
-					<Link
-						key={server.id}
-						href={`/server/${server.id}`}
-						className="bg-background-muted w-full items-center rounded-lg border border-edge bg-background-surface p-3"
-					>
-						<View className="flex-1 items-start justify-center gap-1">
-							<Text className="text-lg">{server.name}</Text>
-							<Text className="text-foreground-muted">
-								{MASK_URLS
-									? server.url.replace(/(https?:\/\/)(.*?)(\/.*)/, '$1$2/...')
-									: server.url}
-							</Text>
-						</View>
-					</Link>
-				))}
-			</View>
+					<View className="flex w-full items-start gap-2">
+						<Text className="text-foreground-muted">OPDS</Text>
 
-			<View className="flex w-full items-start gap-2">
-				<Text className="text-foreground-muted">OPDS</Text>
+						{!opdsServers.length && (
+							<View className="h-24 w-full items-center justify-center gap-2 rounded-lg border border-dashed border-edge p-3">
+								<View className="relative flex justify-center">
+									<View className="flex items-center justify-center rounded-lg bg-background-surface p-2">
+										<Rss className="h-6 w-6 text-foreground-muted" />
+										<Slash className="absolute h-6 w-6 scale-x-[-1] transform text-foreground opacity-80" />
+									</View>
+								</View>
 
-				{!opdsServers.length && (
-					<View className="h-24 w-full items-center justify-center gap-2 rounded-lg border border-dashed border-edge p-3">
-						<View className="relative flex justify-center">
-							<View className="flex items-center justify-center rounded-lg bg-background-surface p-2">
-								<Rss className="h-6 w-6 text-foreground-muted" />
-								<Slash className="absolute h-6 w-6 scale-x-[-1] transform text-foreground opacity-80" />
+								<Text>No OPDS feeds added</Text>
 							</View>
-						</View>
+						)}
 
-						<Text>No OPDS feeds added</Text>
+						{opdsServers.map((server) => (
+							<Link
+								key={server.id}
+								href={`/opds/${server.id}`}
+								className="bg-background-muted w-full items-center rounded-lg border border-edge bg-background-surface p-3"
+							>
+								<View className="flex-1 items-start justify-center gap-1">
+									<Text className="text-lg">{server.name}</Text>
+									<Text className="text-foreground-muted">{formatURL(server.url)}</Text>
+								</View>
+							</Link>
+						))}
 					</View>
-				)}
-
-				{opdsServers.map((server) => (
-					<Link
-						key={server.id}
-						href={`/opds/${server.id}`}
-						className="bg-background-muted w-full items-center rounded-lg border border-edge bg-background-surface p-3"
-					>
-						<View className="flex-1 items-start justify-center gap-1">
-							<Text className="text-lg">{server.name}</Text>
-							<Text className="text-foreground-muted">{formatURL(server.url)}</Text>
-						</View>
-					</Link>
-				))}
-			</View>
-		</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	)
 }

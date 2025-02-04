81500f5981218cb035ea04b79df870304c3cc779
Joseph Micheli <josephwmicheli@gmail.com>
2025-02-04 15:48:24 -0600

    Add todo for after MVP

--- crates/integrations/src/lib.rs
+++ crates/integrations/src/lib.rs
@@ -1,5 +1,7 @@
 #![warn(clippy::dbg_macro)]
 
+//TODO(jmicheli) Reorganize this crate to signal its purpose better. It should probably be called "notifiers" and not have a submodule of the same name.
+
 mod notifier;
 
 pub use notifier::{DiscordClient, Notifier, TelegramClient};

9b07ca8ebb016872f52f349f12f40e9cdc1c0421
darby42 <198700445+darby42@users.noreply.github.com>
2025-03-27 17:28:36 -0700

    Adding tail log api for graphql

--- crates/graphql/src/schema.rs
+++ crates/graphql/src/schema.rs
@@ -1,10 +1,16 @@
-use crate::{data::CoreContext, mutation::Mutation, query::Query};
-use async_graphql::{EmptySubscription, Schema};
+use crate::{
+	data::CoreContext, mutation::Mutation, query::Query, subscription::Subscription,
+};
+use async_graphql::Schema;
 
-pub type AppSchema = Schema<Query, Mutation, EmptySubscription>;
+pub type AppSchema = Schema<Query, Mutation, Subscription>;
 
 pub async fn build_schema(ctx: CoreContext) -> AppSchema {
-	Schema::build(Query::default(), Mutation::default(), EmptySubscription)
-		.data(ctx)
-		.finish()
+	Schema::build(
+		Query::default(),
+		Mutation::default(),
+		Subscription::default(),
+	)
+	.data(ctx)
+	.finish()
 }

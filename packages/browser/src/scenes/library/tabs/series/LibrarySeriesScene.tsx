183e4f26e30c44ddf0b9cff4adb99a12fa26099e
darby42 <198700445+darby42@users.noreply.github.com>
2025-07-20 15:31:23 -0700

    Adding table support for library series, library books, and series book scenes

--- packages/browser/src/scenes/library/tabs/series/LibrarySeriesScene.tsx
+++ packages/browser/src/scenes/library/tabs/series/LibrarySeriesScene.tsx
@@ -27,6 +27,7 @@ import {
 	useURLKeywordSearch,
 	useURLPageParams,
 } from '@/components/filters/useFilterScene'
+import { SeriesTable } from '@/components/series'
 import SeriesGrid from '@/components/series/SeriesGrid'
 import TableOrGridLayout from '@/components/TableOrGridLayout'
 import useIsInView from '@/hooks/useIsInView'
@@ -177,7 +178,7 @@ function LibrarySeriesScene() {
 		}
 	}, [differentSearch, setPage])
 
-	const { layoutMode, setLayout } = useSeriesLayout((state) => ({
+	const { layoutMode, setLayout, columns, setColumns } = useSeriesLayout((state) => ({
 		columns: state.columns,
 		layoutMode: state.layout,
 		setColumns: state.setColumns,
@@ -233,6 +234,67 @@ function LibrarySeriesScene() {
 		[shouldScroll, isInView],
 	)
 
+	const renderContent = () => {
+		if (layoutMode === 'GRID') {
+			return (
+				<URLFilterContainer
+					currentPage={pageInfo.currentPage || 1}
+					pages={pageInfo.totalPages || 1}
+					onChangePage={(page) => {
+						setPage(page)
+					}}
+					onPrefetchPage={(page) => {
+						prefetch(id, {
+							page,
+							pageSize,
+							filter: filters,
+							orderBy,
+						})
+					}}
+				>
+					<div className="flex flex-1 px-4 pb-2 pt-4 md:pb-4">
+						<SeriesGrid
+							isLoading={isLoading}
+							series={nodes}
+							hasFilters={Object.keys(filters || {}).length > 0}
+						/>
+					</div>
+				</URLFilterContainer>
+			)
+		} else {
+			return (
+				<SeriesTable
+					items={nodes || []}
+					render={(props) => (
+						<URLFilterContainer
+							currentPage={pageInfo.currentPage || 1}
+							pages={pageInfo.totalPages || 1}
+							onChangePage={(page) => {
+								setPage(page)
+							}}
+							onPrefetchPage={(page) => {
+								prefetch(id, {
+									page,
+									pageSize,
+									filter: filters,
+									orderBy,
+								})
+							}}
+							// tableControls={
+							// 	<EntityTableColumnConfiguration
+							// 		entity="series"
+							// 		configuration={columns || defaultColumnSort}
+							// 		onSave={setColumns}
+							// 	/>
+							// }
+							{...props}
+						/>
+					)}
+				/>
+			)
+		}
+	}
+
 	return (
 		<FilterContext.Provider
 			value={{
@@ -258,173 +320,8 @@ function LibrarySeriesScene() {
 					navOffset
 				/>
 
-				<URLFilterContainer
-					currentPage={pageInfo.currentPage || 1}
-					pages={pageInfo.totalPages || 1}
-					onChangePage={(page) => {
-						setPage(page)
-					}}
-					onPrefetchPage={(page) => {
-						prefetch(id, {
-							page,
-							pageSize,
-							filter: filters,
-							orderBy,
-						})
-					}}
-				>
-					<div className="flex flex-1 px-4 pb-2 pt-4 md:pb-4">
-						<SeriesGrid
-							isLoading={isLoading}
-							series={nodes}
-							hasFilters={Object.keys(filters || {}).length > 0}
-						/>
-					</div>
-				</URLFilterContainer>
+				{renderContent()}
 			</div>
 		</FilterContext.Provider>
 	)
 }
-
-// const { layoutMode, setLayout } = useSeriesLayout((state) => ({
-// 	layoutMode: state.layout,
-// 	setLayout: state.setLayout,
-// }))
-// const { isLoading, library } = useLibraryByID(id)
-// const {
-// 	filters,
-// 	ordering,
-// 	pagination: { page, page_size },
-// 	setPage,
-// 	...rest
-// } = useFilterScene()
-// const { prefetch } = usePrefetchPagedSeries()
-
-// const params = useMemo(
-// 	() => ({
-// 		page,
-// 		page_size,
-// 		params: {
-// 			...filters,
-// 			...ordering,
-// 			count_media: true,
-// 			library: {
-// 				id: [id],
-// 			},
-// 		},
-// 	}),
-// 	[page, page_size, filters, ordering, id],
-// )
-// const {
-// 	isLoading: isLoadingSeries,
-// 	isRefetching: isRefetchingSeries,
-// 	series,
-// 	pageData,
-// } = usePagedSeriesQuery(params)
-// const { current_page, total_pages } = pageData || {}
-
-// const differentSearch = usePreviousIsDifferent(filters?.search as string)
-// useEffect(() => {
-// 	if (differentSearch) {
-// 		setPage(1)
-// 	}
-// }, [differentSearch, setPage])
-
-// const handlePrefetchPage = useCallback(
-// 	(page: number) => {
-// 		prefetch({
-// 			...params,
-// 			page,
-// 		})
-// 	},
-// 	[params, prefetch],
-// )
-
-// const previousPage = usePrevious(current_page)
-// const shouldScroll = !!previousPage && previousPage !== current_page
-// useEffect(
-// 	() => {
-// 		if (!isInView && shouldScroll) {
-// 			containerRef.current?.scrollIntoView({
-// 				behavior: 'smooth',
-// 				block: 'nearest',
-// 				inline: 'start',
-// 			})
-// 		}
-// 	},
-// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-// 	[isInView, shouldScroll],
-// )
-
-// if (isLoading) {
-// 	return null
-// } else if (!library) {
-// 	throw new Error('Library not found')
-// }
-
-// const renderContent = () => {
-// 	if (layoutMode === 'GRID') {
-// 		return (
-// 			<URLFilterContainer
-// 				currentPage={current_page || 1}
-// 				pages={total_pages || 1}
-// 				onChangePage={setPage}
-// 				onPrefetchPage={handlePrefetchPage}
-// 			>
-// 				<div className="flex flex-1 px-4 pb-2 pt-4 md:pb-4">
-// 					<SeriesGrid
-// 						isLoading={isLoadingSeries}
-// 						series={series}
-// 						hasFilters={Object.keys(filters || {}).length > 0}
-// 					/>
-// 				</div>
-// 			</URLFilterContainer>
-// 		)
-// 	} else {
-// 		return (
-// 			<SeriesTable
-// 				items={series || []}
-// 				render={(props) => (
-// 					<URLFilterContainer
-// 						currentPage={current_page || 1}
-// 						pages={total_pages || 1}
-// 						onChangePage={setPage}
-// 						onPrefetchPage={handlePrefetchPage}
-// 						// tableControls={<BookTableColumnConfiguration />}
-// 						{...props}
-// 					/>
-// 				)}
-// 			/>
-// 		)
-// 	}
-// }
-
-// return (
-// 	<FilterContext.Provider
-// 		value={{
-// 			filters,
-// 			ordering,
-// 			pagination: { page, page_size },
-// 			setPage,
-// 			...rest,
-// 		}}
-// 	>
-// 		<div className="flex flex-1 flex-col pb-4 md:pb-0">
-// 			<Helmet>
-// 				<title>Stump | {library.name}</title>
-// 			</Helmet>
-
-// 			<section ref={containerRef} id="grid-top-indicator" className="h-0" />
-
-// 			<FilterHeader
-// 				isSearching={isRefetchingSeries}
-// 				layoutControls={<TableOrGridLayout layout={layoutMode} setLayout={setLayout} />}
-// 				orderControls={<URLOrdering entity="series" />}
-// 				filterControls={<URLFilterDrawer entity="series" />}
-// 				navOffset
-// 			/>
-
-// 			{renderContent()}
-// 		</div>
-// 	</FilterContext.Provider>
-// )

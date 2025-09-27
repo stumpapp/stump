f4ce4b0d115ca2496bd6d54c5516a6f7809cffcc
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2025-09-26 21:09:11 -0700

    fix stale name for custom theme

--- apps/expo/components/book/reader/epub/controls/CustomizeTheme.tsx
+++ apps/expo/components/book/reader/epub/controls/CustomizeTheme.tsx
@@ -31,6 +31,7 @@ export default function CustomizeTheme({ onCancel }: Props) {
 	useEffect(
 		() => {
 			setCustomTheme(resolveTheme(themes, selectedTheme || '', colorScheme))
+			setName(resolveThemeName(themes, selectedTheme || '', colorScheme))
 		},
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 		[selectedTheme],

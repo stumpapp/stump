1c9e801609dd8019805767c6e2fbe8570d9bd39c
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2025-10-12 14:42:42 -0700

    :bug: Fix API key copy for insecure protocols (i.e., http)
    Relates to an issue identified as part of #799

--- packages/components/src/hooks/useCopyToClipboard.ts
+++ packages/components/src/hooks/useCopyToClipboard.ts
@@ -1,15 +1,18 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCopyToClipboard as useClipboard } from 'react-use'
 
 export function useCopyToClipboard(data: string, duration = 2000) {
 	const [copied, setCopied] = useState(false)
 
 	const timeoutRef = useRef<NodeJS.Timeout | null>(null)
 
+	const [, copyToClipboard] = useClipboard()
+
 	const copy = useCallback(async () => {
-		await navigator.clipboard.writeText(data)
+		await copyToClipboard(data)
 		setCopied(true)
 		timeoutRef.current = setTimeout(() => setCopied(false), duration)
-	}, [data, duration])
+	}, [data, duration, copyToClipboard])
 
 	useEffect(() => {
 		return () => {

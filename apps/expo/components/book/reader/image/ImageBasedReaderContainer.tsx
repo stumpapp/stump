67dd623cc4a1f7ec6b8463fde2bc65dbca3c14bc
Jo <141064017+Arklaum@users.noreply.github.com>
2025-10-19 22:38:09 +0100

    New Performance Monitor

--- apps/expo/components/book/reader/image/ImageBasedReaderContainer.tsx
+++ apps/expo/components/book/reader/image/ImageBasedReaderContainer.tsx
@@ -1,13 +1,10 @@
 import { ReadingMode } from '@stump/graphql'
 import { generatePageSets, ImageBasedBookPageRef } from '@stump/sdk'
-import { ComponentProps, useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { AppState, View } from 'react-native'
+import { ComponentProps, useCallback, useMemo, useRef, useState } from 'react'
+import { View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
-import { PerformanceMonitor } from 'react-native-reanimated'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { useDisplay } from '~/lib/hooks'
-import { usePreferencesStore } from '~/stores'
 import { DEFAULT_BOOK_PREFERENCES, useBookPreferences } from '~/stores/reader'
 
 import { IImageBasedReaderContext, ImageBasedReaderContext, NextInSeriesBookRef } from './context'
@@ -38,9 +35,6 @@ export default function ImageBasedReaderContainer({
 			secondPageSeparate,
 		},
 	} = useBookPreferences({ book: ctx.book, serverId: ctx.serverId })
-
-	const inset = useSafeAreaInsets()
-	const performanceMonitor = usePreferencesStore((state) => state.performanceMonitor)
 
 	const [imageSizes, setImageSizes] = useState<Record<number, ImageBasedBookPageRef>>(
 		() =>
@@ -112,20 +106,6 @@ export default function ImageBasedReaderContainer({
 	// 	[initialPage],
 	// )
 
-	// Note: we must not render the PerformanceMonitor when the app is not active:
-	// e.g. when you open the iOS Control Center or Notification Center
-	// or else the app will freeze
-	const [isAppFocused, setIsAppFocused] = useState(true)
-	useEffect(() => {
-		if (!performanceMonitor) return
-		const subscription = AppState.addEventListener('change', (nextAppState) =>
-			setIsAppFocused(nextAppState === 'active'),
-		)
-		return () => {
-			subscription.remove()
-		}
-	}, [performanceMonitor])
-
 	return (
 		<ImageBasedReaderContext.Provider
 			value={{
@@ -138,11 +118,6 @@ export default function ImageBasedReaderContainer({
 				flatListRef,
 			}}
 		>
-			{performanceMonitor && isAppFocused && (
-				<View className="absolute inset-0 items-center" style={{ top: inset.top }}>
-					<PerformanceMonitor />
-				</View>
-			)}
 			<View className="fixed inset-0 flex-1 bg-black">
 				<ControlsOverlay />
 

1fe42add5e72c762c7c9a55bd1d2b5c86cbe0c0b
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2025-12-26 09:26:04 -0700

    :bento: Add more app artwork

--- apps/expo/components/ServerConnectFailed.tsx
+++ apps/expo/components/ServerConnectFailed.tsx
@@ -1,9 +1,9 @@
 import { useRouter } from 'expo-router'
-import { WifiOff } from 'lucide-react-native'
 import { View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-import { Button, Heading, Icon, Text } from './ui'
+import Owl, { useOwlHeaderOffset } from './Owl'
+import { Button, Heading, Text } from './ui'
 
 type Props = {
 	onRetry?: () => void
@@ -11,34 +11,42 @@ type Props = {
 
 export default function ServerConnectFailed({ onRetry }: Props) {
 	const router = useRouter()
+	const emptyContainerStyle = useOwlHeaderOffset()
 
 	return (
-		<SafeAreaView className="flex-1 bg-background p-4">
-			<View className="flex-1 gap-4">
-				<Heading size="xl" className="text-center">
-					Failed to Connect
-				</Heading>
+		<SafeAreaView className="flex-1 bg-background">
+			<View
+				className="h-full flex-1 items-center justify-center gap-8 p-4"
+				style={emptyContainerStyle}
+			>
+				<Owl owl="network-error" />
 
-				<View className="relative flex flex-row justify-center">
-					<View className="squircle flex items-center justify-center rounded-lg bg-background-surface p-2">
-						<Icon as={WifiOff} className="h-10 w-10 text-foreground-muted" />
-					</View>
+				<View className="gap-2 px-4 tablet:max-w-lg">
+					<Heading size="xl" className="text-center font-semibold leading-tight">
+						Failed to Connect
+					</Heading>
+
+					<Text size="lg" className="text-center">
+						A network error suggests this server is currently unavailable. Please ensure that it is
+						running and accessible from this device
+					</Text>
 				</View>
 
-				<Text size="lg" className="text-center">
-					A network error suggests this server is currently unavailable. Please ensure that it is
-					running and accessible from this device
-				</Text>
+				<View className="flex-1" />
 
-				<View />
-
-				<View className="flex-row justify-center">
-					<Button variant="brand" onPress={() => router.dismissAll()}>
+				<View className="w-full gap-3">
+					<Button variant="brand" size="lg" roundness="full" onPress={() => router.dismissAll()}>
 						<Text>Return Home</Text>
 					</Button>
 
 					{onRetry && (
-						<Button variant="secondary" className="ml-2" onPress={onRetry}>
+						<Button
+							variant="secondary"
+							size="lg"
+							roundness="full"
+							className="ml-2"
+							onPress={onRetry}
+						>
 							<Text>Try Again</Text>
 						</Button>
 					)}

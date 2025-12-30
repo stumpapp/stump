5c8364777273013c14bf73cfd2493c324bcd4618
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2025-12-29 17:50:43 -0700

    use server name for home

--- apps/expo/app/server/[id]/(tabs)/index/_layout.tsx
+++ apps/expo/app/server/[id]/(tabs)/index/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from 'expo-router'
 import { Platform } from 'react-native'
 
+import { useActiveServer } from '~/components/activeServer'
 import { IS_IOS_24_PLUS } from '~/lib/constants'
 import { usePreferencesStore } from '~/stores'
 
 export default function Layout() {
+	const {
+		activeServer: { name: serverName },
+	} = useActiveServer()
+
 	const animationEnabled = usePreferencesStore((state) => !state.reduceAnimations)
 
 	return (
@@ -13,7 +18,7 @@ export default function Layout() {
 				name="index"
 				options={{
 					headerShown: true,
-					headerTitle: 'Home',
+					headerTitle: serverName,
 					headerTransparent: Platform.OS === 'ios',
 					headerLargeTitleStyle: {
 						fontSize: 30,

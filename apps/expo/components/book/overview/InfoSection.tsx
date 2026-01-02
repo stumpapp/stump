6faa62831958f273ca189f734e37795b424dd14c
Jo <141064017+Arklaum@users.noreply.github.com>
2026-01-02 20:50:22 +0000

    update InfoSection design

--- apps/expo/components/book/overview/InfoSection.tsx
+++ apps/expo/components/book/overview/InfoSection.tsx
@@ -1,7 +1,6 @@
 import { View } from 'react-native'
 
-import { Text } from '~/components/ui'
-import { cn } from '~/lib/utils'
+import { Card, CardDivider, Text } from '~/components/ui'
 
 type Props = {
 	label: string
@@ -12,17 +11,14 @@ export default function InfoSection({ label, rows }: Props) {
 	return (
 		<View className="flex w-full gap-2">
 			<Text className="text-lg text-foreground-muted">{label}</Text>
-			<View className="squircle flex flex-col rounded-lg border border-edge bg-background-surface">
+			<Card>
 				{rows.map((row, index) => (
 					<View key={`section-${label}-${index}`}>
 						{row}
-						{index < rows.length - 1 && <Divider />}
+						{index < rows.length - 1 && <CardDivider />}
 					</View>
 				))}
-			</View>
+			</Card>
 		</View>
 	)
 }
-
-// TODO: Not full width on iOS like native list?
-const Divider = () => <View className={cn('h-px w-full bg-edge')} />

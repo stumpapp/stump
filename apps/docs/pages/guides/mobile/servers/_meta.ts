7258db40a791359a852286c7ffe718426d6891ef
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-01-13 08:39:17 -0700

    :memo: Update Codex in mobile docs

---
+++ apps/docs/pages/guides/mobile/servers/_meta.ts
@@ -0,0 +1,8 @@
+import { Meta } from 'nextra'
+
+export default {
+	index: 'Overview',
+	authentication: 'Authentication',
+	configuration: 'Configuration',
+	'opds-compatibility': 'OPDS Compatibility',
+} satisfies Meta

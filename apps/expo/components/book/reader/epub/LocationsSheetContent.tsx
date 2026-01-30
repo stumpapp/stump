4ddee6121f6ab687692bbfbdc8b0abf598709d00
Jo <141064017+Arklaum@users.noreply.github.com>
2026-01-30 22:48:39 +0000

    scroll to active chapter

--- apps/expo/components/book/reader/epub/LocationsSheetContent.tsx
+++ apps/expo/components/book/reader/epub/LocationsSheetContent.tsx
@@ -1,8 +1,11 @@
+import { FlashList, FlashListRef, ViewToken } from '@shopify/flash-list'
 import { getColor, serialize } from 'colorjs.io/fn'
-import { useRef, useState } from 'react'
+import { GlassView } from 'expo-glass-effect'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { Platform, Pressable, useWindowDimensions, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import PagerView from 'react-native-pager-view'
+import Animated, { Easing, Keyframe } from 'react-native-reanimated'
 import { stripHtml } from 'string-strip-html'
 
 import { ThumbnailImage } from '~/components/image'
@@ -18,6 +21,8 @@ import AnnotationsAndBookmarks from './AnnotationsAndBookmarks'
 
 export default function LocationsSheetContent() {
 	const [activePage, setActivePage] = useState(0)
+	const [visibleRange, setVisibleRange] = useState({ min: 0, max: 0 })
+
 	const { height: windowHeight } = useWindowDimensions()
 
 	const pagerHeight =
@@ -25,11 +30,14 @@ export default function LocationsSheetContent() {
 		72 - // py-6 + text(ish)
 		60 // tabs
 
-	const ref = useRef<PagerView>(null)
+	const pagerViewRef = useRef<PagerView>(null)
+	const flashListRef = useRef<FlashListRef<TableOfContentsItemWithLevel>>(null)
 
 	const book = useEpubLocationStore((store) => store.book)
 	const toc = useEpubLocationStore((store) => store.toc)
 	const embeddedMetadata = useEpubLocationStore((store) => store.embeddedMetadata)
+	const position = useEpubLocationStore((store) => store.position)
+	const currentChapter = useEpubLocationStore((store) => store.currentChapter)
 
 	const requestHeaders = useEpubLocationStore((store) => store.requestHeaders)
 
@@ -39,12 +47,74 @@ export default function LocationsSheetContent() {
 	const bookAuthor = book?.metadata?.writers?.join(', ') || embeddedMetadata?.author
 	const bookPublisher = book?.metadata?.publisher || embeddedMetadata?.publisher
 
+	const flatTocWithLevels = flattenTocWithLevels(toc)
+
+	const findNextItem = (item: TableOfContentsItem) => {
+		const flatToc = flattenToc(toc)
+		const index = flatToc.indexOf(item)
+		return flatToc[index + 1]
+	}
+
+	const checkIsActive = (item: TableOfContentsItem) => {
+		const nextItem = findNextItem(item)
+		if (item.position) {
+			const isAfterChapterStart = position >= item.position
+			const isBeforeChapterEnd = nextItem?.position ? position < nextItem?.position : true
+			return isAfterChapterStart && isBeforeChapterEnd
+		}
+		return item.label === currentChapter
+	}
+
+	const activeTocItemIndex = flatTocWithLevels.findIndex(({ item }) => checkIsActive(item))
+
+	const scrollToCurrentChapter = useCallback(
+		({ animated }: { animated: boolean }) =>
+			flashListRef.current?.scrollToIndex({
+				index: activeTocItemIndex,
+				animated: animated,
+				viewPosition: 0.5,
+				// each row is 49px, and we scroll back up a bit to make it look more balanced
+				viewOffset: 49 / 2,
+			}),
+		[activeTocItemIndex],
+	)
+
+	// we initially put the active chapter in the middle using this effect since
+	// initialScrollIndex does not have a `viewPosition` equivalent
+	useEffect(() => {
+		scrollToCurrentChapter({ animated: false })
+	}, [scrollToCurrentChapter])
+
+	// flash the scrollbar to give a rough indication of where we are
+	useEffect(() => {
+		if (activePage === 1) {
+			setTimeout(() => {
+				flashListRef.current?.flashScrollIndicators()
+			}, 250)
+		}
+	}, [activePage, scrollToCurrentChapter])
+
+	const showTopIndicator = activeTocItemIndex < visibleRange.min
+	const showBottomIndicator = activeTocItemIndex > visibleRange.max
+
+	const onViewableItemsChanged = useCallback(
+		({ viewableItems }: { viewableItems: ViewToken<TableOfContentsItemWithLevel>[] }) => {
+			if (viewableItems.length > 0) {
+				setVisibleRange({
+					min: viewableItems.at(0)?.index ?? 0,
+					max: viewableItems.at(-1)?.index ?? 0,
+				})
+			}
+		},
+		[],
+	)
+
 	if (!book) return
 
 	return (
 		<View className="flex-1 gap-1">
 			<View className="flex-row items-center justify-around px-4 py-6">
-				<Pressable onPress={() => ref.current?.setPage(0)}>
+				<Pressable onPress={() => pagerViewRef.current?.setPage(0)}>
 					{({ pressed }) => (
 						<Text
 							className={cn('text-lg font-medium text-foreground-subtle', {
@@ -57,7 +127,7 @@ export default function LocationsSheetContent() {
 					)}
 				</Pressable>
 
-				<Pressable onPress={() => ref.current?.setPage(1)}>
+				<Pressable onPress={() => pagerViewRef.current?.setPage(1)}>
 					{({ pressed }) => (
 						<Text
 							className={cn('text-lg font-medium text-foreground-subtle', {
@@ -70,7 +140,7 @@ export default function LocationsSheetContent() {
 					)}
 				</Pressable>
 
-				<Pressable onPress={() => ref.current?.setPage(2)}>
+				<Pressable onPress={() => pagerViewRef.current?.setPage(2)}>
 					{({ pressed }) => (
 						<Text
 							className={cn('text-lg font-medium text-foreground-subtle', {
@@ -85,7 +155,7 @@ export default function LocationsSheetContent() {
 			</View>
 
 			<PagerView
-				ref={ref}
+				ref={pagerViewRef}
 				style={{ flex: 1, height: pagerHeight }}
 				initialPage={0}
 				onPageSelected={(e) => setActivePage(e.nativeEvent.position)}
@@ -131,19 +201,37 @@ export default function LocationsSheetContent() {
 					</ScrollView>
 				</View>
 
-				<View
-					style={{
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-					key="2"
-				>
-					<ScrollView className="w-full" contentContainerStyle={{ paddingBottom: 16 }}>
-						{toc?.map((item) => (
-							<TableOfContentsListItem key={item.label} item={item} />
-						))}
-					</ScrollView>
+				<View key="2">
+					<FlashList
+						ref={flashListRef}
+						data={flatTocWithLevels}
+						contentContainerStyle={{ paddingBottom: 16 }}
+						onViewableItemsChanged={onViewableItemsChanged}
+						renderItem={({ item, index }) => (
+							<TableOfContentsListItem
+								item={item.item}
+								level={item.level}
+								currentChapterActive={index === activeTocItemIndex}
+								nextChapterActive={index + 1 === activeTocItemIndex}
+							/>
+						)}
+					/>
+
+					{showTopIndicator && (
+						<ScrollToChapterIndicator
+							onPress={() => scrollToCurrentChapter({ animated: true })}
+							className="top-4"
+						/>
+					)}
+
+					{showBottomIndicator && (
+						<ScrollToChapterIndicator
+							onPress={() => scrollToCurrentChapter({ animated: true })}
+							className="bottom-6"
+						/>
+					)}
 				</View>
+
 				<View
 					style={{
 						justifyContent: 'center',
@@ -161,16 +249,15 @@ export default function LocationsSheetContent() {
 const TableOfContentsListItem = ({
 	item,
 	level = 0,
-	className,
+	currentChapterActive,
+	nextChapterActive,
 }: {
 	item: TableOfContentsItem
 	level?: number
-	className?: string
+	currentChapterActive: boolean
+	nextChapterActive: boolean
 }) => {
 	const actions = useEpubLocationStore((store) => store.actions)
-	const currentChapter = useEpubLocationStore((store) => store.currentChapter)
-	const position = useEpubLocationStore((store) => store.position)
-	const toc = useEpubLocationStore((store) => store.toc)
 	const closeSheet = useEpubSheetStore((state) => state.closeSheet)
 
 	const handlePress = async () => {
@@ -199,29 +286,6 @@ const TableOfContentsListItem = ({
 	color.alpha = isDarkColorScheme ? (isChild ? 0.5 : 0.8) : isChild ? 0.7 : 0.9
 	const textColor = serialize(color, { format: 'hex' })
 
-	const findNextItem = (item: TableOfContentsItem) => {
-		const flatToc = flattenToc(toc)
-		const index = flatToc.indexOf(item)
-		return flatToc[index + 1]
-	}
-
-	const checkIsActive = (item: TableOfContentsItem) => {
-		const nextItem = findNextItem(item)
-		if (item.position) {
-			const isAfterChapterStart = position >= item.position
-			const isBeforeChapterEnd = nextItem?.position ? position < nextItem?.position : true
-			return isAfterChapterStart && isBeforeChapterEnd
-		}
-		return item.label === currentChapter
-	}
-
-	// Hide Divider if:
-	// 1. next item is active (hides Divider above active item)
-	// 2. current item is active (hides Divider below active item)
-	const nextItem = findNextItem(item)
-	const currentChapterActive = checkIsActive(item)
-	const nextChapterActive = nextItem ? checkIsActive(nextItem) : false
-
 	return (
 		<View>
 			<Pressable onPress={handlePress}>
@@ -240,7 +304,7 @@ const TableOfContentsListItem = ({
 							style={{ opacity: pressed ? 0.7 : 1, paddingLeft: 16 + level * 16, paddingRight: 16 }}
 						>
 							<Text
-								className={cn('py-4 text-base', currentChapterActive && 'font-bold', className)}
+								className={cn('py-4 text-base', currentChapterActive && 'font-bold')}
 								style={currentChapterActive && { color: textColor }}
 							>
 								{item.label}
@@ -260,16 +324,6 @@ const TableOfContentsListItem = ({
 			</Pressable>
 
 			{!nextChapterActive && !currentChapterActive && <Divider level={level} />}
-
-			{item.children.map((child) => (
-				<View key={child.label}>
-					<TableOfContentsListItem
-						item={child}
-						level={level + 1}
-						className="text-foreground-muted"
-					/>
-				</View>
-			))}
 		</View>
 	)
 }
@@ -286,3 +340,69 @@ const Divider = ({ level = 0 }: { level?: number }) => (
 		}}
 	/>
 )
+
+// GlassView doesn't like zero opacity https://github.com/expo/expo/issues/41024
+const enteringAnimation = new Keyframe({
+	from: { opacity: 0.02 },
+	to: { opacity: 1, easing: Easing.inOut(Easing.quad) },
+}).duration(350)
+
+const exitingAnimation = new Keyframe({
+	from: { opacity: 1 },
+	to: { opacity: 0.02, easing: Easing.inOut(Easing.quad) },
+}).duration(350)
+
+const ScrollToChapterIndicator = ({
+	onPress,
+	className,
+}: {
+	onPress: () => void
+	className?: string
+}) => {
+	const accentColor = usePreferencesStore((state) => state.accentColor)
+	const colors = useColors()
+	const textColor = accentColor || colors.fill.brand.DEFAULT
+
+	return (
+		<Animated.View
+			entering={enteringAnimation}
+			exiting={exitingAnimation}
+			className={cn('absolute left-0 right-0 items-center', className)}
+		>
+			<Pressable onPress={onPress}>
+				<GlassView
+					glassEffectStyle="regular"
+					style={{ borderRadius: 999 }}
+					isInteractive
+					// this is for android only, but ios ignores it so it's fine
+					className="bg-background-surface"
+				>
+					<View className="px-4 py-2">
+						<Text className="text-base font-semibold" style={{ color: textColor }}>
+							Show Current Chapter
+						</Text>
+					</View>
+				</GlassView>
+			</Pressable>
+		</Animated.View>
+	)
+}
+
+type TableOfContentsItemWithLevel = { item: TableOfContentsItem; level: number }
+
+const flattenTocWithLevels = (
+	toc: TableOfContentsItem[],
+	level = 0,
+): TableOfContentsItemWithLevel[] => {
+	const flatTocWithLevels = toc.reduce((acc, item) => {
+		acc.push({ item, level })
+
+		if (item.children && item.children.length > 0) {
+			acc.push(...flattenTocWithLevels(item.children, level + 1))
+		}
+
+		return acc
+	}, [] as TableOfContentsItemWithLevel[])
+
+	return flatTocWithLevels
+}

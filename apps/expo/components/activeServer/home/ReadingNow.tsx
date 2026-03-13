027c79ee12c9cf69808c480ce9e6fa9d4084127e
Jo <141064017+Arklaum@users.noreply.github.com>
2026-03-13 11:35:42 +0000

    fix reading now progress bar

--- apps/expo/components/activeServer/home/ReadingNow.tsx
+++ apps/expo/components/activeServer/home/ReadingNow.tsx
@@ -334,7 +334,7 @@ function ReadingNowItem({ book }: ReadingNowItemProps) {
 							</Text>
 						</View>
 
-						{percentageCompleted && (
+						{percentageCompleted != null && (
 							<Progress
 								className="h-1 bg-[#898d94]"
 								indicatorClassName="bg-[#f5f3ef]"

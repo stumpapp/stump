393b7df34f55e06121c4637229f61e80738fa7b7
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-05-24 12:38:41 -0700

    add drizzle migration for expo

--- apps/expo/drizzle/migrations.js
+++ apps/expo/drizzle/migrations.js
@@ -6,6 +6,7 @@ import m0001 from './0001_unknown_manta.sql';
 import m0002 from './0002_jazzy_charles_xavier.sql';
 import m0003 from './0003_magenta_hardball.sql';
 import m0004 from './0004_happy_odin.sql';
+import m0005 from './0005_low_lady_ursula.sql';
 
   export default {
     journal,
@@ -14,7 +15,8 @@ import m0004 from './0004_happy_odin.sql';
 m0001,
 m0002,
 m0003,
-m0004
+m0004,
+m0005
     }
   }
   

d525b00a2ec4da9a077c993dd837523a7a2e30eb
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-05-24 16:29:15 -0700

    switches

--- packages/components/src/input/raw/RawSwitch.tsx
+++ packages/components/src/input/raw/RawSwitch.tsx
@@ -5,25 +5,25 @@ import React from 'react'
 
 import { cn } from '../../utils'
 
-// TODO: figure out the contrast colors for default colors...
-
 export const SWITCH_BASE_CLASSES = [
-	'inline-flex shrink-0 cursor-pointer items-center rounded-full transition-colors',
-	'border border-transparent',
-	'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-background-400 focus:ring-offset-background',
+	'peer group/switch relative inline-flex shrink-0 items-center rounded-full border border-transparent transition-all outline-none',
+	'after:absolute after:-inset-x-3 after:-inset-y-2',
+	'focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50',
+	'aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/30',
+	'data-[state=checked]:bg-primary data-[state=unchecked]:bg-switch',
 	'disabled:cursor-not-allowed disabled:opacity-50',
-	'data-[state=unchecked]:bg-background-surface-secondary',
 ]
 
 export const SWITCH_SIZE_VARIANTS = {
-	default: 'h-[24px] w-[44px]',
-	sm: 'h-[20px] w-[36px]',
-	xs: 'h-[16px] w-[28px]',
+	default: 'h-[18.4px] w-8',
+	sm: 'h-3.5 w-6',
+	// TODO(shad): remove
+	xs: 'h-4 w-7',
 }
-// TODO(tailwind): Once I migrate to new tailwind and redo theming I need a switch background token for themes
+// TODO(shad): remove
 export const SWITCH_VARIANTS = {
-	default: 'focus:ring-background-400 data-[state=checked]:bg-background-surface-secondary-hover',
-	primary: 'focus:ring-edge-brand data-[state=checked]:bg-fill-brand',
+	default: '',
+	primary: '',
 }
 export const switchVariants = cva(SWITCH_BASE_CLASSES, {
 	defaultVariants: {
@@ -39,6 +39,7 @@ export const switchVariants = cva(SWITCH_BASE_CLASSES, {
 export type RawSwitchRef = ElementRef<typeof SwitchPrimitives.Root>
 export type RawSwitchProps = VariantProps<typeof switchVariants> &
 	ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> & {
+		// TODO(shad): remove
 		primaryRing?: boolean
 	}
 
@@ -47,24 +48,24 @@ export const RawSwitch = React.forwardRef<RawSwitchRef, RawSwitchProps>(
 		return (
 			<SwitchPrimitives.Root
 				className={cn(switchVariants({ className, size, variant }), {
-					'focus:ring-edge-brand': primaryRing,
+					'focus-visible:ring-ring/60': primaryRing,
 				})}
 				{...props}
 				ref={ref}
 			>
 				<SwitchPrimitives.Thumb
 					className={cn(
-						'bg-white shadow-lg pointer-events-none block rounded-full ring-0 transition-transform',
+						'pointer-events-none block rounded-full ring-0 transition-transform data-[state=checked]:bg-switch-thumb-checked data-[state=unchecked]:bg-switch-thumb',
 						{
-							'h-5 w-5 data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0':
+							'data-[state=unchecked]:translate-x-0 size-4 data-[state=checked]:translate-x-[calc(100%-2px)]':
 								size === 'default',
 						},
 						{
-							'h-4 w-4 data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0':
+							'size-3 data-[state=unchecked]:translate-x-0 data-[state=checked]:translate-x-[calc(100%-2px)]':
 								size === 'sm',
 						},
 						{
-							'h-3 w-3 data-[state=checked]:translate-x-3 data-[state=unchecked]:translate-x-0.5':
+							'size-3 data-[state=unchecked]:translate-x-0 data-[state=checked]:translate-x-[calc(100%-2px)]':
 								size === 'xs',
 						},
 					)}

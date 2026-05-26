d3343d07b596321774dfe5e4ce303db9d0ef0ea5
Jo <141064017+Arklaum@users.noreply.github.com>
2026-05-26 18:46:59 +0100

    working background colour

--- apps/expo/app/server/[id]/books/index.tsx
+++ apps/expo/app/server/[id]/books/index.tsx
@@ -1,9 +1,11 @@
-import { FlashList } from '@shopify/flash-list'
+import { FlashList, FlashListRef, ViewToken } from '@shopify/flash-list'
 import { useInfiniteGraphQL, useRefetch, useSuspenseGraphQL } from '@stump/client'
-import { graphql } from '@stump/graphql'
+import { graphql, InterfaceLayout } from '@stump/graphql'
 import { keepPreviousData } from '@tanstack/react-query'
-import { useCallback, useRef } from 'react'
+import { MeshGradientView } from 'expo-mesh-gradient'
+import { useCallback, useEffect, useRef } from 'react'
 import { Platform } from 'react-native'
+import Animated, { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useStore } from 'zustand'
 import { useShallow } from 'zustand/react/shallow'
@@ -57,6 +59,8 @@ const statsQuery = graphql(`
 	}
 `)
 
+const AnimatedMeshGradientView = Animated.createAnimatedComponent(MeshGradientView)
+
 export default function Screen() {
 	const {
 		activeServer: { id: serverID },
@@ -98,6 +102,47 @@ export default function Screen() {
 		},
 	)
 
+	const layout = useBooksLayout('global', (state) => state.layout)
+	const { numColumns, paddingHorizontal, ItemSeparatorComponent } = useListSizing({ layout })
+
+	const isGrid = layout === InterfaceLayout.Grid
+
+	// TODO: what to start with? keep as is (fades colours in), or use estimate lastItem with itemHeight + ItemSeparatorComponent height.
+	const firstColor = useSharedValue('transparent')
+	const lastColor = useSharedValue('transparent')
+
+	const animatedProps = useAnimatedProps(() => {
+		return { colors: [firstColor.value, firstColor.value, lastColor.value, lastColor.value] }
+	})
+
+	const flashListRef = useRef<FlashListRef<any>>(null)
+
+	const onViewableItemsChanged = useCallback(
+		({ viewableItems }: { viewableItems: ViewToken<any>[] }) => {
+			if (viewableItems.length > 0) {
+				const scrollOffset = flashListRef.current?.getAbsoluteLastScrollOffset() ?? 0
+				// we don't want the first visible item because that's often under the header
+				// TODO: but just selecting the second item isn't a very accurate way to do it
+				const firstIndex = scrollOffset <= 0 || isGrid ? 0 : 1
+
+				const firstItem = viewableItems.at(firstIndex)?.item
+				const lastItem = viewableItems.at(-1)?.item
+
+				//TODO: use color-js for better light + dark colours
+				const newFirstColor = firstItem?.thumbnail?.metadata?.averageColor + 'c0' || 'transparent'
+				const newLastColor = lastItem?.thumbnail?.metadata?.averageColor + 'c0' || 'transparent'
+
+				firstColor.set(withTiming(newFirstColor, { duration: 800 }))
+				lastColor.set(withTiming(newLastColor, { duration: 800 }))
+			}
+		},
+		[firstColor, lastColor, isGrid],
+	)
+
+	useEffect(() => {
+		flashListRef.current?.recomputeViewableItems()
+	}, [filters, layout, sort])
+
 	const refetch = () => Promise.all([refetchBooks(), refetchStats()])
 
 	const [isRefetching, onRefetch] = useRefetch(refetch)
@@ -110,16 +155,26 @@ export default function Screen() {
 
 	const isFiltered = Object.keys(filters).length > 0
 
-	const layout = useBooksLayout('global', (state) => state.layout)
-	const { numColumns, paddingHorizontal, ItemSeparatorComponent } = useListSizing({ layout })
-
 	return (
 		<BookFilterContext.Provider value={store}>
 			<SafeAreaView
 				style={{ flex: 1 }}
 				edges={['left', 'right', ...(Platform.OS === 'ios' ? [] : ['bottom' as const])]}
 			>
+				<AnimatedMeshGradientView
+					columns={2}
+					rows={2}
+					points={[
+						[0, 0],
+						[1, 0],
+						[0, 1],
+						[1, 1],
+					]}
+					animatedProps={animatedProps}
+					style={{ position: 'absolute', inset: 0 }}
+				/>
 				<FlashList
+					ref={flashListRef}
 					key={layout} // force re-render when layout changes
 					data={data?.pages.flatMap((page) => page.media.nodes) || []}
 					renderItem={({ item }) => <BookListItem layout={layout} book={item} />}
@@ -130,6 +185,11 @@ export default function Screen() {
 					numColumns={numColumns}
 					onEndReachedThreshold={ON_END_REACHED_THRESHOLD}
 					onEndReached={onEndReached}
+					onViewableItemsChanged={onViewableItemsChanged}
+					viewabilityConfig={{
+						itemVisiblePercentThreshold: isGrid ? 70 : 30,
+						minimumViewTime: 800,
+					}}
 					contentInsetAdjustmentBehavior="automatic"
 					ListHeaderComponent={<BooksListHeader stats={booksStats} />}
 					ListHeaderComponentStyle={{ paddingBottom: 16, marginHorizontal: -paddingHorizontal }}

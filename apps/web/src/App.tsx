45ce89ead0d0c477b41788f2a6034018db734dcf
Balázs Szücs <bszucs1209@gmail.com>
2026-05-29 19:42:26 +0200

    Revert changes to apps/web/src/App.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,12 @@
 import { StumpWebClient } from '@stump/browser'
 import { BrowserRouter } from 'react-router-dom'
 
-function getLocation() {
-	if (typeof globalThis === 'undefined' || !('location' in globalThis)) {
-		return null
-	}
-
-	return globalThis.location
-}
-
 const getDebugUrl = () => {
-	const hostname = getLocation()?.hostname || 'localhost'
+	const { hostname } = window.location
 	return `http://${hostname}:10801`
 }
 
-export const baseUrl = import.meta.env.PROD ? (getLocation()?.origin ?? '') : getDebugUrl()
+export const baseUrl = import.meta.env.PROD ? window.location.origin : getDebugUrl()
 
 export default function App() {
 	return (

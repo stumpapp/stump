92b8dac1ae01bacc74902d3c2a39325e74e8176f
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-05-30 10:34:01 -0700

    rm outdated comment

--- apps/server/tests/api_tests.rs
+++ apps/server/tests/api_tests.rs
@@ -4,8 +4,6 @@ mod reading_progress;
 
 use common::TestApp;
 
-// TODO(testing): prob split into separate folders for groups, e.g. auth/ opds/ etc
-
 /// server should start, the first user can register and login successfully
 #[tokio::test]
 async fn test_server_boots_and_auth_works() {

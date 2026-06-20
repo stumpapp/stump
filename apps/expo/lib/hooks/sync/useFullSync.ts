068d451ba89e33cfff5a769dc57244205f7e6143
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-06-20 11:40:09 -0700

    wip: collect instances once before sync ops
    its a little bit of an awkward dance where each sync type has its own hook to implement how sync works, then full sync uses them all in sequence. the problem is they each pulled instances and would report on skipped servers. the more ideal fix is outlined in a comment for future aaron problem

--- apps/expo/lib/hooks/sync/useFullSync.ts
+++ apps/expo/lib/hooks/sync/useFullSync.ts
@@ -3,6 +3,7 @@ import { useCallback } from 'react'
 import { useAnnotationSync } from './useAnnotationSync'
 import { useBookmarkSync } from './useBookmarkSync'
 import { useProgressSync } from './useProgressSync'
+import { useServerInstances } from './utils'
 
 /**
  * A hook that combines progress, bookmark, and annotation syncing
@@ -11,12 +12,15 @@ export function useFullSync() {
 	const { syncProgress, pushProgress, pullProgress } = useProgressSync()
 	const { syncBookmarks, pushBookmarks, pullBookmarks } = useBookmarkSync()
 	const { syncAnnotations, pushAnnotations, pullAnnotations } = useAnnotationSync()
+	const { getInstances } = useServerInstances()
 
 	const syncAll = useCallback(
 		async (forServers?: string[]) => {
-			const progressResults = await syncProgress(forServers)
-			const bookmarkResults = await syncBookmarks(forServers)
-			const annotationResults = await syncAnnotations(forServers)
+			const instances = await getInstances(forServers)
+
+			const progressResults = await syncProgress({ forServers, instances })
+			const bookmarkResults = await syncBookmarks({ forServers, instances })
+			const annotationResults = await syncAnnotations({ forServers, instances })
 
 			return {
 				progress: progressResults,
@@ -24,14 +28,16 @@ export function useFullSync() {
 				annotations: annotationResults,
 			}
 		},
-		[syncProgress, syncBookmarks, syncAnnotations],
+		[getInstances, syncProgress, syncBookmarks, syncAnnotations],
 	)
 
 	const pushAll = useCallback(
 		async (forServers?: string[]) => {
-			const progressResults = await pushProgress({ forServers })
-			const bookmarkResults = await pushBookmarks({ forServers })
-			const annotationResults = await pushAnnotations({ forServers })
+			const instances = await getInstances(forServers)
+
+			const progressResults = await pushProgress({ forServers, instances })
+			const bookmarkResults = await pushBookmarks({ forServers, instances })
+			const annotationResults = await pushAnnotations({ forServers, instances })
 
 			return {
 				progress: progressResults,
@@ -39,14 +45,16 @@ export function useFullSync() {
 				annotations: annotationResults,
 			}
 		},
-		[pushProgress, pushBookmarks, pushAnnotations],
+		[getInstances, pushProgress, pushBookmarks, pushAnnotations],
 	)
 
 	const pullAll = useCallback(
 		async (forServers?: string[]) => {
-			const progressResults = await pullProgress(forServers)
-			const bookmarkResults = await pullBookmarks(forServers)
-			const annotationResults = await pullAnnotations(forServers)
+			const instances = await getInstances(forServers)
+
+			const progressResults = await pullProgress({ forServers, instances })
+			const bookmarkResults = await pullBookmarks({ forServers, instances })
+			const annotationResults = await pullAnnotations({ forServers, instances })
 
 			return {
 				progress: progressResults,
@@ -54,7 +62,7 @@ export function useFullSync() {
 				annotations: annotationResults,
 			}
 		},
-		[pullProgress, pullBookmarks, pullAnnotations],
+		[getInstances, pullProgress, pullBookmarks, pullAnnotations],
 	)
 
 	return {

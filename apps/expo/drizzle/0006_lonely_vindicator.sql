e148c700d3cfd3a211adadeba82f807a40c8d7f6
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-08-01 10:26:45 -0700

    replace migration

--- apps/expo/drizzle/0006_lonely_vindicator.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `read_progress` ADD `conflict_data` text;--> statement-breakpoint
-ALTER TABLE `read_progress` ADD `last_pulled_session_updated_at` integer;--> statement-breakpoint
-ALTER TABLE `read_progress` ADD `pending_reset` integer DEFAULT false NOT NULL;

ded4a53e4599dab3361c6531163e977b691aedad
Santiago Morales <santiago.mora.dev@gmail.com>
2026-08-16 14:59:49 -0500

    :wrench: (browser): Configure Jest for Readium ESM-only packages

--- packages/browser/jest.config.ts
+++ packages/browser/jest.config.ts
@@ -3,11 +3,22 @@ import type { Config } from 'jest'
 export default {
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
+		'^@readium/decorator$': '<rootDir>/../../node_modules/@readium/decorator/dist/index.js',
+		'^@readium/helpers$': '<rootDir>/../../node_modules/@readium/helpers/dist/index.js',
+		'^@readium/navigator$': '<rootDir>/../../node_modules/@readium/navigator/dist/index.js',
+		'^@readium/navigator-html-injectables$':
+			'<rootDir>/../../node_modules/@readium/navigator-html-injectables/dist/index.js',
+		'^@readium/shared$': '<rootDir>/../../node_modules/@readium/shared/dist/index.js',
 	},
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 	modulePathIgnorePatterns: ['<rootDir>/dist/'],
 	testEnvironment: 'jsdom',
 	transform: {
-		'^.+\\.tsx?$': 'babel-jest',
+		'^.+\\.[jt]sx?$': 'babel-jest',
 	},
+	// Readium's ESM bundles include nested ESM dependencies under their own
+	// node_modules directories, so the entire package subtree must be transformed.
+	transformIgnorePatterns: [
+		'^(?!.*[/\\\\]node_modules[/\\\\]@readium[/\\\\]).*[/\\\\]node_modules[/\\\\]',
+	],
 } satisfies Config

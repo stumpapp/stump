a67286184945cd0599ae5c2bfe8219e7f6ce017c
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-08-24 16:38:39 -0700

    fix desktop and expo lints
    desktop is so behind :sob:

--- apps/desktop/src/components/createOrUpdate/__tests__/schema.test.ts
+++ apps/desktop/src/components/createOrUpdate/__tests__/schema.test.ts
@@ -1,10 +1,12 @@
-import { FORBIDDEN_ENTITY_NAMES } from '@/utils/form'
+import { FORBIDDEN_ENTITY_NAMES } from '@stump/browser/utils'
 
 import { buildSchema, CreateOrUpdateServerSchema } from '../schema'
 
 const validServer: CreateOrUpdateServerSchema = {
 	name: 'name',
-	uri: 'https://example.com',
+	url: 'https://example.com',
+	isDefault: false,
+	authMode: 'login',
 }
 
 const createSavedServer = (
@@ -26,11 +28,11 @@ describe('CreateOrUpdateServerSchema', () => {
 		it('should not allow existing names', () => {
 			const schema = buildSchema([createSavedServer()], vi.fn())
 			expect(
-				schema.safeParse(createSavedServer({ name: 'name', uri: 'https://newexample.com' }))
+				schema.safeParse(createSavedServer({ name: 'name', url: 'https://newexample.com' }))
 					.success,
 			).toBe(false)
 			expect(
-				schema.safeParse(createSavedServer({ name: 'newName', uri: 'https://newexample.com' }))
+				schema.safeParse(createSavedServer({ name: 'newName', url: 'https://newexample.com' }))
 					.success,
 			).toBe(true)
 		})
@@ -38,24 +40,30 @@ describe('CreateOrUpdateServerSchema', () => {
 		it('should not allow existing URIs', () => {
 			const schema = buildSchema([createSavedServer()], vi.fn())
 			expect(
-				schema.safeParse(createSavedServer({ name: 'newName', uri: 'https://example.com' }))
+				schema.safeParse(createSavedServer({ name: 'newName', url: 'https://example.com' }))
 					.success,
 			).toBe(false)
 			expect(
-				schema.safeParse(createSavedServer({ name: 'newName', uri: 'https://newexample.com' }))
+				schema.safeParse(createSavedServer({ name: 'newName', url: 'https://newexample.com' }))
 					.success,
 			).toBe(true)
 		})
 
 		describe('update', () => {
 			it('should allow the same name as itself', () => {
-				const schema = buildSchema([createSavedServer()], vi.fn(), createSavedServer())
+				const schema = buildSchema([createSavedServer()], vi.fn(), {
+					id: 'foo',
+					...createSavedServer(),
+				})
 				expect(schema.safeParse(createSavedServer({ name: 'name' })).success).toBe(true)
 			})
 
-			it('should allow the same URI as itself', () => {
-				const schema = buildSchema([createSavedServer()], vi.fn(), createSavedServer())
-				expect(schema.safeParse(createSavedServer({ uri: 'https://example.com' })).success).toBe(
+			it('should allow the same url as itself', () => {
+				const schema = buildSchema([createSavedServer()], vi.fn(), {
+					id: 'foo',
+					...createSavedServer(),
+				})
+				expect(schema.safeParse(createSavedServer({ url: 'https://example.com' })).success).toBe(
 					true,
 				)
 			})

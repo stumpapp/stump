4ab07d348675ebe2e9c8399c204f432c5b8c0831
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-09-05 18:12:38 -0700

    copy over pdf
    did fewer refactors because IM TIRED

--- core/src/media/processor/error.rs
+++ core/src/media/processor/error.rs
@@ -6,12 +6,24 @@ pub enum MediaProcessorError {
 	BlockingTask(#[from] tokio::task::JoinError),
 	#[error("The media file was not found")]
 	FileNotFound,
+	#[error("The media file is too small to process")]
+	FileTooSmall,
 	#[error("An IO error occurred: {0}")]
 	Io(#[from] std::io::Error),
 	#[error("Failed to read image size: {0}")]
 	ImageSize(#[from] imagesize::ImageError),
+	#[error("Image operation failed: {0}")]
+	Image(#[from] image::ImageError),
+	#[error("PDFium is not configured or available")]
+	PdfConfiguration,
+	#[error("A PDFium error occurred: {0}")]
+	Pdfium(#[from] pdfium_render::prelude::PdfiumError),
+	#[error("Failed to render PDF page")]
+	PdfRenderFailed,
 	#[error("The page requested was not found in the media file")]
 	PageNotFound,
+	#[error("An unknown error occurred: {0}")]
+	Unknown(String),
 	#[error("An unrar error occurred: {0}")]
 	Unrar(#[from] unrar::error::UnrarError),
 	#[error("This file type is not supported: {0}")]

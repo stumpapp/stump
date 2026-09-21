e700d3e6b7593086cc6bb36ee3099c0c7d5a2d9f
Aaron Leopold <36278431+aaronleopold@users.noreply.github.com>
2026-09-21 15:01:36 -0700

    🩹 (expo) Fix external link taps for Android ebooks

--- apps/expo/modules/contentUriInfo/android/src/main/java/expo/modules/contenturiinfo/ContentUriInfoModule.kt
+++ apps/expo/modules/contentUriInfo/android/src/main/java/expo/modules/contenturiinfo/ContentUriInfoModule.kt
@@ -6,25 +6,26 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 class ContentUriInfoModule : Module() {
-    override fun definition() = ModuleDefinition {
-        Name("ContentUriInfo")
+    override fun definition() =
+        ModuleDefinition {
+            Name("ContentUriInfo")
 
-        AsyncFunction("getDisplayName") { uriString: String ->
-            val contentResolver = appContext.reactContext?.contentResolver
-            if (contentResolver == null) {
-                null
-            } else {
-                runCatching {
-                    contentResolver.query(Uri.parse(uriString), arrayOf(OpenableColumns.DISPLAY_NAME), null, null, null)?.use { cursor ->
-                        val index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
-                        if (index >= 0 && cursor.moveToFirst()) {
-                            cursor.getString(index)
-                        } else {
-                            null
+            AsyncFunction("getDisplayName") { uriString: String ->
+                val contentResolver = appContext.reactContext?.contentResolver
+                if (contentResolver == null) {
+                    null
+                } else {
+                    runCatching {
+                        contentResolver.query(Uri.parse(uriString), arrayOf(OpenableColumns.DISPLAY_NAME), null, null, null)?.use { cursor ->
+                            val index = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                            if (index >= 0 && cursor.moveToFirst()) {
+                                cursor.getString(index)
+                            } else {
+                                null
+                            }
                         }
-                    }
-                }.getOrNull()
+                    }.getOrNull()
+                }
             }
         }
-    }
 }
